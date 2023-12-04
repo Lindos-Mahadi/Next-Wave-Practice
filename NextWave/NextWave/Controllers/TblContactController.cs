c899472ca5b16239c46a89d739d3440ad56a5bde
Lindos-Mahadi <lindosmahadi@gmail.com>
2023-12-04 14:43:15 +0600

    TblContact List Fetch data

---
+++ NextWave/NextWave/Controllers/TblContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NextWave.Models;
+
+namespace NextWave.Controllers
+{
+    public class TblContactController : Controller
+    {
+        private readonly NextWaveDbContext _demoContext;
+
+        public TblContactController(NextWaveDbContext demoContext)
+        {
+            _demoContext = demoContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetContactList()
+        { 
+            var contactList = _demoContext.TblContacts.ToList().Take(20);
+            return Json(contactList);
+        }
+    }
+}
