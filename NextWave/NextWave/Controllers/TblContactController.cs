708123dfe17463fca77c4da70750815f55d783aa
Lindos-Mahadi <lindosmahadi@gmail.com>
2023-12-05 19:23:02 +0600

    Trying to Datatable using sorting, Searching, filtering but not complete

--- NextWave/NextWave/Controllers/TblContactController.cs
+++ NextWave/NextWave/Controllers/TblContactController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NextWave.Models;
+using System.Linq.Dynamic.Core;
+using System.Net.NetworkInformation;
 
 namespace NextWave.Controllers
 {
@@ -16,11 +19,86 @@ namespace NextWave.Controllers
         {
             return View();
         }
+
+
         [HttpGet]
-        public ActionResult GetContactList()
-        { 
-            var contactList = _demoContext.TblContacts.ToList().Take(20);
-            return Json(contactList);
+        public JsonResult GetContactList(string searchValue, string sortOrder, int start, int length)
+        {
+            try
+            {
+                var query = _demoContext.TblContacts.AsQueryable();
+                // Total records count
+                int totalRecord = query.Count();
+                // Filtering based on search value
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    query = query.Where(x => x.Name.ToLower().Contains(searchValue.ToLower()) || x.Name.ToUpper().Contains(searchValue.ToUpper()));
+                }
+
+                // Filtered records count
+                int filterRecord = query.Count();
+                // Sorting
+                //if (!string.IsNullOrEmpty(sortOrder))
+                //{
+                //    // Assuming sortOrder is the name of the property to sort on
+                //    query = query.OrderByProperty(sortOrder, ascending: true);
+                //}
+                // Paging
+                var data = query.Skip(start).Take(length).ToList();
+
+                return Json(new { data = data, recordsTotal = totalRecord, recordsFiltered = filterRecord });
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
         }
+        //public ActionResult GetContactList(string searchValue)
+        //{
+        //    int totalRecord = 0;
+        //    int filterRecord = 0;
+        //    var data = _demoContext.TblContacts.AsQueryable();
+        //    totalRecord = data.Count();
+        //    if (!string.IsNullOrEmpty(searchValue))
+        //    {
+        //        data = data.Where(x => x.Name.ToLower().Contains(searchValue.ToLower()));
+        //    }
+        //    filterRecord = data.Count();
+
+        //    return Json(new { data = data, recordsTotal = totalRecord, recordsFiltered = filterRecord });
+        //}
+        //public ActionResult GetContactList(string searchValue, string sortOrder, int start, int length)
+        //{
+        //    // Initial query
+        //    var query = _demoContext.TblContacts.AsQueryable();
+
+        //    // Total records count
+        //    int totalRecord = query.Count();
+
+        //    // Filtering based on search value
+        //    if (!string.IsNullOrEmpty(searchValue))
+        //    {
+        //        query = query.Where(x => x.Name.ToLower().Contains(searchValue.ToLower()));
+        //    }
+
+        //    // Filtered records count
+        //    int filterRecord = query.Count();
+
+        //    // Sorting
+        //    //if (!string.IsNullOrEmpty(sortOrder))
+        //    //{
+        //    //    // Assuming sortOrder is the name of the property to sort on
+        //    //    query = query.OrderByProperty(sortOrder, ascending: true);
+        //    //}
+
+        //    // Paging
+        //    var data = query.Skip(start).Take(length).ToList();
+
+        //    return Json(new { data = data, recordsTotal = totalRecord, recordsFiltered = filterRecord });
+        //}
+
     }
+
 }
